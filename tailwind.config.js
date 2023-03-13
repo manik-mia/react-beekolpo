f5ce7498a2934eb778ffcf80a942e3513d72927e
Manik Mia <mohammadmanikmia@yahoo.com>
2023-03-14 03:55:29 +0600

    Some Change on Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,10 +24,6 @@ module.exports = {
             center: true,
             padding: {
                 DEFAULT: "1rem",
-                sm: "2rem",
-                lg: "4rem",
-                xl: "5rem",
-                "2xl": "6rem",
             },
         },
     },
